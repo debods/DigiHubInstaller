534f710f2864c3cbbab866435928d10fc86c44b4
debods <57447490+debods@users.noreply.github.com>
2025-12-15 16:54:11 -0500

    Update hamgrid.py

--- Files/pyscripts/hamgrid.py
+++ Files/pyscripts/hamgrid.py
@@ -14,30 +14,36 @@ Output: Maidenhead Grid Square
 
 import math
 import argparse
+import validcoords
 
 def grid_conversion(latitude, longitude):
- field_lon = int((longitude + 180) / 20)
- field_lat = int((latitude + 90) / 10)
- grid = chr(ord('A') + field_lon) + chr(ord('A') + field_lat)
+    field_lon = int((longitude + 180) / 20)
+    field_lat = int((latitude + 90) / 10)
+    grid = chr(ord('A') + field_lon) + chr(ord('A') + field_lat)
 
- square_lon = int(((longitude + 180) % 20) / 2)
- square_lat = int(((latitude + 90) % 10) / 1)
- grid += str(square_lon) + str(square_lat)
+    square_lon = int(((longitude + 180) % 20) / 2)
+    square_lat = int(((latitude + 90) % 10) / 1)
+    grid += str(square_lon) + str(square_lat)
 
- subsquare_lon = int((((longitude + 180) % 20) % 2) / (2/24))
- subsquare_lat = int((((latitude + 90) % 10) % 1) / (1/24))
- grid += chr(ord('A') + subsquare_lon) + chr(ord('A') + subsquare_lat)
+    subsquare_lon = int((((longitude + 180) % 20) % 2) / (2/24))
+    subsquare_lat = int((((latitude + 90) % 10) % 1) / (1/24))
+    grid += chr(ord('A') + subsquare_lon) + chr(ord('A') + subsquare_lat)
 
- return grid
+    return grid
 
-parser = argparse.ArgumentParser(description="Calculate Maidenhead Grid from Latitude and Longitude.")
+parser = argparse.ArgumentParser(
+    description="Calculate Maidenhead Grid from Latitude and Longitude."
+)
 parser.add_argument('latitude', type=float, help="Latitude (e.g., 41.714649)")
 parser.add_argument('longitude', type=float, help="Longitude (e.g., -72.728485)")
 args = parser.parse_args()
 
-maidenhead = grid_conversion(args.latitude, args.longitude)
-prefix = maidenhead[:-2]
-suffix = maidenhead[-2:].lower()
-maidenhead = prefix+suffix
+try:
+    validcoords.validate(args.latitude, args.longitude)
+except ValueError as e:
+    parser.error(str(e))
 
-print(f"{maidenhead}")
+maidenhead = grid_conversion(args.latitude, args.longitude)
+maidenhead = maidenhead[:-2] + maidenhead[-2:].lower()
+
+print(maidenhead)
